1ade2c042ff5dd368501e4f3c1a5a8c112a1312b
Greg Shiner <greg@gregshiner.com>
2025-07-20 22:18:27 -0400

    Remove ocaml

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -88,13 +88,6 @@ require('lazy').setup({
   --    For additional information see: :help lazy.nvim-lazy.nvim-structuring-your-plugins
 
   { import = 'custom/plugins' },
-
-  -- Lazy load ocaml plugin
-  {
-    dir = 'vimscript/ocaml.vim',
-    ft = 'ocaml',
-    lazy = true,
-  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
